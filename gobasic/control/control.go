4b2bb72fa05466b6ab9c198e02ba5813d969489a
Perry He <wantnotshould@qq.com>
2025-12-20 17:47:13 +0800

    函数、方法、接口、数据结构这些好像在01_基础就有写过了，先暂时保留 02_流程控制 和 03_并发编程吧

---
+++ gobasic/control/control.go
@@ -0,0 +1,6 @@
+// Package control 02_流程控制
+package control
+
+func Control(){
+	
+}
--- gobasic/main.go
+++ gobasic/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "learn-go/gobasic/basic"
+import (
+	"learn-go/gobasic/basic"
+	"learn-go/gobasic/control"
+)
 
 func main() {
 	// 01_基础
 	basic.Basic()
+	// 02_流程控制
+	control.Control()
 }
